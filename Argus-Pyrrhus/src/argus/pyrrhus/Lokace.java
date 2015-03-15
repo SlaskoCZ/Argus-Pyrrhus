3c97fdf64e7e5a56237e917ddd7ce17f4edb3b36
Slasko <Slasko@Slasko-PC>
2015-03-15 15:10:34 +0100

    Vytvoren class lokace a mapa

---
+++ Argus-Pyrrhus/src/argus/pyrrhus/Lokace.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package argus.pyrrhus;
+
+/**
+ *
+ * @author Slasko
+ */
+public class Lokace {
+void louka(){
+    System.out.println("Prisel si na louku, v dali vidis"+Enemy.enemyName);
+    System.out.println("1. priblizit se");
+    System.out.println("2. zautocit");
+    System.out.println("3. odejit");
+    
+}    
+}
---
+++ Argus-Pyrrhus/src/argus/pyrrhus/Mapa.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package argus.pyrrhus;
+
+/**
+ *
+ * @author Slasko
+ */
+public class Mapa {
+
+    void mapgen(){
+        int[][][] mapa = new int[25][25][2];
+        
+    }
+}
