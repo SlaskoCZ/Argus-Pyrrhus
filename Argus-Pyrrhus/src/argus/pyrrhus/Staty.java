78f9ac4317121bc61ede359a74fad9a2d1c9e035
Slasko <Slasko@Slasko-PC>
2015-03-15 13:03:59 +0100

    Uprava starych promennych a vytvoreni hitu pro enemy

--- Argus-Pyrrhus/src/argus/pyrrhus/Staty.java
+++ Argus-Pyrrhus/src/argus/pyrrhus/Staty.java
@@ -16,13 +16,12 @@ import java.util.Scanner;
  */
 public class Staty {
     public static int zdravi;
-    public static int critical;
-    public static float sance;
+    public static int hit;
     void name(){
     //zdraví
     zdravi=VIT*5;
     //critical sance
-    sance = ((float)ArgusPyrrhus.DEX * (0.5f));
+    float sance = ((float)ArgusPyrrhus.DEX * (0.5f));
     int critical;
     int random= (int)(1 + 100* Math.random());
        if ((100-sance) > random) {
@@ -32,10 +31,24 @@ public class Staty {
            critical=1;
         }
     //síla utoku
-       int odchylka = (int)(1.1 - 0.2* Math.random());
-    int ATK =(int) (ArgusPyrrhus.STR * odchylka * critical);
+    int odchylka = (int)(1.1 - 0.2* Math.random());
+    hit =(int) (ArgusPyrrhus.STR * odchylka * critical);
     
 
    
     
-}}
+}
+    void enemyHit(){
+        int enemyCritical;
+        float enemySance = ((float)Enemy.enemyDEX * (0.5f));
+       int enemyRandom= (int)(1 + 100* Math.random());
+       if ((100-enemySance) > enemyRandom) {
+           enemyCritical=2;
+            
+        } else {
+           enemyCritical=1;
+        }
+    int odchylka = (int)(1.1 - 0.2* Math.random());
+    Enemy.enemyHit =(int) (Enemy.enemySTR * odchylka * enemyCritical);   
+    }
+}
