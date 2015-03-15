d2128767894f1fae079c4671eb82eae0f9273bfe
Slasko <Slasko@Slasko-PC>
2015-03-15 01:37:24 +0100

    1. Commit

---
+++ Argus-Pyrrhus/src/argus/pyrrhus/ArgusPyrrhus.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package argus.pyrrhus;
+
+import java.io.Console;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Slasko
+ */
+public class ArgusPyrrhus {
+           public static String name;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner user_input;
+               user_input = new Scanner(System.in);
+        Zobrazeni zobrazeni;
+               zobrazeni = new Zobrazeni();
+        Console console;
+               console = System.console();
+
+        //Jmeno
+            System.out.println("Zadej jmmeno hrdiny!");
+            name = user_input.next();
+            
+        // Vypis do konzole
+            zobrazeni.JmenoHrdiny();
+     
+       
+                
+    }
+  
+   
+}
