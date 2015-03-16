c678c1ad3a6f80e6103bb5d23de93f85c9460729
Slasko <Slasko@Slasko-PC>
2015-03-16 21:06:38 +0100

    Vytvoreno menu 1. Hlavni menu 2. Presunuto Rozcesti 3. Presunuta obtiznost

---
+++ Argus-Pyrrhus/src/argus/pyrrhus/Menu.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package argus.pyrrhus;
+
+import static argus.pyrrhus.ArgusPyrrhus.obtiznost;
+import static argus.pyrrhus.Mapa.heroMap;
+import static argus.pyrrhus.Mapa.heroPos;
+
+/**
+ *
+ * @author Slasko
+ */
+public class Menu {
+
+    Mapa mapa = new Mapa();
+    Vstup vstup = new Vstup();
+    Utilities utilities = new Utilities();
+    Lokace lokace = new Lokace();
+    Shop shop = new Shop();
+    Souboj souboj = new Souboj();
+    Enemy enemy = new Enemy();
+
+    //Hlavni menu
+    void hlavniMenu() {
+        System.out.println("1. Mapa ");
+        System.out.println("1. Shop ");
+        System.out.println("1. Rychly Souboj");
+        System.out.println("1. ");
+        System.out.println("5.Ukoncit");
+        System.out.print("Tvoje volba: ");
+        vstup.vstup();
+        switch (Integer.valueOf(Vstup.vstup)) {
+            case 0:
+                System.out.println("Testovaci rezim");
+                break;
+            case 1:
+                rozcesti();
+                break;
+            case 2:
+                shop.nakup();
+                break;
+            case 3:
+                enemy.getEnemy((int) (Math.round(Math.random() * 4 + 1)));
+                souboj.Boj();
+                break;
+            case 4:
+                break;
+            case 5:
+                System.exit(1);
+                break;
+        }
+
+    }
+
+    //Rozcesti od 1. Mapa
+
+    void rozcesti() {
+        System.out.println("");
+        System.out.println("Rozcesti:");
+        System.out.println("1.Nahoru");
+        System.out.println("2.Dolu");
+        System.out.println("3.Doprava");
+        System.out.println("4.Doleva");
+        System.out.println("5.Zpet do menu");
+        System.out.println("6.Vypis mapya");
+        System.out.println("7.Shop");
+        System.out.print("Tvoje volba: ");
+        vstup.vstup();
+        int x = Integer.valueOf(Vstup.vstup);
+        boolean newPosition = false;
+        switch (x) {
+            case 1:
+                heroPos[0]--;
+                newPosition = true;
+                break;
+            case 2:
+                heroPos[0]++;
+                newPosition = true;
+                break;
+            case 3:
+                heroPos[1]++;
+                newPosition = true;
+                break;
+            case 4:
+                heroPos[1]--;
+                newPosition = true;
+                break;
+            case 5:
+                hlavniMenu();
+                break;
+            case 6:
+                mapa.mapVision();
+                break;
+            case 7:
+                shop.nakup();
+                break;
+            default:
+                System.exit(1);
+                break;
+
+        }
+
+    }
+
+    
+        void obtiznost() {
+    System.out.println("Obtiznost (Easy,Normal,Hard,Impossible)");
+        System.out.print("Zadej obtiznost: ");
+        vstup.vstup();
+        switch (Vstup.vstup) {
+            case "Easy":
+                obtiznost = 0.5;
+                break;
+            case "Normal":
+                obtiznost = 1;
+                break;
+            case "Hard":
+                obtiznost = 1.5;
+                break;
+            case "Impossible":
+                obtiznost = 2;
+                break;
+            case "Test":
+                obtiznost = 0.1;
+                break;
+            default:
+                obtiznost = 1;
+                break;
+        }
+    }
+}
+
+
