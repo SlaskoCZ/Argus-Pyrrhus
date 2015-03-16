727869bb27441b98f97a0d430861dec1fdbedf75
Slasko <Slasko@Slasko-PC>
2015-03-16 14:57:57 +0100

    upraven vstup

--- Argus-Pyrrhus/src/argus/pyrrhus/Shop.java
+++ Argus-Pyrrhus/src/argus/pyrrhus/Shop.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package argus.pyrrhus;
+
 import static argus.pyrrhus.ArgusPyrrhus.money;
 
 /**
@@ -11,55 +12,56 @@ import static argus.pyrrhus.ArgusPyrrhus.money;
  * @author Karel
  */
 public class Shop {
+
     Vstup vstup = new Vstup();
     Databazeitemu databazeitemu = new Databazeitemu();
     Shop shop = new Shop();
     int vyber;
     int cena;
-    
- public static Boolean payment;   
- void nakup(){
-          vstup.vstupint(); 
- switch (Vstup.vstup){
-     case "1":
-     cena = 25;
-     System.out.println("Opravdu chcete koupit tento item za"+cena+" ?[ano/ne]");
-     vstup.vstup();
-     switch (Vstup.vstup) {
-         case "ano":
-             if (money-cena>0) {
-              payment=true;
-              money-=cena;
-     databazeitemu.HP();break; 
-     
-             }
-     
-         case "ne":
-             shop.nakup();break;
-             
-     case "2":
-     cena = 25;
-     System.out.println("Opravdu chcete koupit tento item za"+cena+" ?[ano/ne]");
-     vstup.vstup();
-     switch (Vstup.vstup) {
-         case "ano":
-             if (money-cena>0) {
-              payment=true;
-              money-=cena;
-     databazeitemu.HP();break; 
-     
-             }
-     
-         case "ne":
-             shop.nakup();break;        
-             
-             
 
-      
-   
- }}}}}   
- 
- 
-    
-    
+    public static Boolean payment;
 
+    void nakup() {
+        vstup.vstup();
+        switch (Integer.valueOf(Vstup.vstup)) {
+            case "1":
+                cena = 25;
+                System.out.println("Opravdu chcete koupit tento item za" + cena + " ?[ano/ne]");
+                vstup.vstup();
+                switch (Vstup.vstup) {
+                    case "ano":
+                        if (money - cena > 0) {
+                            payment = true;
+                            money -= cena;
+                            databazeitemu.HP();
+                            break;
+
+                        }
+
+                    case "ne":
+                        shop.nakup();
+                        break;
+
+                    case "2":
+                        cena = 25;
+                        System.out.println("Opravdu chcete koupit tento item za" + cena + " ?[ano/ne]");
+                        vstup.vstup();
+                        switch (Vstup.vstup) {
+                            case "ano":
+                                if (money - cena > 0) {
+                                    payment = true;
+                                    money -= cena;
+                                    databazeitemu.HP();
+                                    break;
+
+                                }
+
+                            case "ne":
+                                shop.nakup();
+                                break;
+
+                        }
+                }
+        }
+    }
+}
