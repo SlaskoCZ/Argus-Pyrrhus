7e4f694d470348f57b30fbfe9edf11e7d95d588c
Slasko <Slasko@Slasko-PC>
2015-03-16 21:07:01 +0100

    Obtiznost presunuta do Menu kvuli uhlednosti

--- Argus-Pyrrhus/src/argus/pyrrhus/ArgusPyrrhus.java
+++ Argus-Pyrrhus/src/argus/pyrrhus/ArgusPyrrhus.java
@@ -36,15 +36,9 @@ public class ArgusPyrrhus {
      */
     public static void main(String[] args) throws IOException, InterruptedException {
         Vstup vstup = new Vstup();
-        Zobrazeni zobrazeni = new Zobrazeni();
         Utilities utilities = new Utilities();
-        Lokace lokace = new Lokace();
-        Souboj souboj = new Souboj();
         Menu menu = new Menu();
         Mapa mapa = new Mapa();
-        Shop shop = new Shop();
-        Enemy enemy = new Enemy();
-        int i = 0;
         //Mapgen
         mapa.mapgen();
 
@@ -54,29 +48,7 @@ public class ArgusPyrrhus {
         //Jmeno
         vstup.name();
         //Obtiznost
-        System.out.println("Obtiznost (Easy,Normal,Hard,Impossible)");
-        System.out.print("Zadej obtiznost: ");
-        vstup.vstup();
-        switch (Vstup.vstup) {
-            case "Easy":
-                obtiznost = 0.5;
-                break;
-            case "Normal":
-                obtiznost = 1;
-                break;
-            case "Hard":
-                obtiznost = 1.5;
-                break;
-            case "Impossible":
-                obtiznost = 2;
-                break;
-            case "Test":
-                obtiznost = 0.1;
-                break;
-            default:
-                obtiznost = 1;
-                break;
-        }
+        menu.obtiznost();
         //Pozice
         Mapa.heroMap[13][13] = "X";
         Mapa.heroPos[0] = 13;
