29a3b587caa2c056ba7f0269315605fa77b26216
Slasko <Slasko@Slasko-PC>
2015-03-17 18:31:21 +0100

    Pridan vstup pro obchod z textoveho souboru

--- Argus-Pyrrhus/src/argus/pyrrhus/Vstup.java
+++ Argus-Pyrrhus/src/argus/pyrrhus/Vstup.java
@@ -14,6 +14,22 @@ import static argus.pyrrhus.ArgusPyrrhus.VIT;
 import static argus.pyrrhus.ArgusPyrrhus.LVL;
 import static argus.pyrrhus.ArgusPyrrhus.EXP;
 import static argus.pyrrhus.ArgusPyrrhus.name;
+import java.io.BufferedInputStream;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -23,7 +39,6 @@ import java.util.Scanner;
 public class Vstup {
 
     public static String vstup;
-    
 
     void name() {
         Scanner user_input;
@@ -35,10 +50,10 @@ public class Vstup {
     }
 
     void stats() {
-        STR = 10+ArgusPyrrhus.itemstr;
-        DEX = 10+ArgusPyrrhus.itemdex;
+        STR = 10 + ArgusPyrrhus.itemstr;
+        DEX = 10 + ArgusPyrrhus.itemdex;
         INT = 10;
-        VIT = 10+ArgusPyrrhus.itemvit;
+        VIT = 10 + ArgusPyrrhus.itemvit;
         LVL = 1;
         EXP = 0;
         zdravi = VIT * 5;
@@ -50,5 +65,42 @@ public class Vstup {
         user_input = new Scanner(System.in);
         vstup = user_input.next();
 
-   
-}}
+    }
+    private final static Charset ENCODING = StandardCharsets.UTF_8;
+
+    public String[] shopItemy() throws IOException {
+        String path = ("D:\\skola\\Java\\ja\\Argus-Pyrrhus\\Argus-Pyrrhus\\Test_Files\\Test_File.txt");
+        FileReader fileReader = new FileReader(path);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        int numberOfLines = countLines(path);
+        String[] polozky = new String[numberOfLines];
+        String line = null;
+        int j = 0;
+        int nOfIt = 0;
+        for (int i = 0; i < numberOfLines; i++) {
+            polozky[j] = bufferedReader.readLine();
+
+            if (polozky[j].contains("##")) {
+                j--;
+            } else {
+                nOfIt++;
+            }
+            j++;
+
+        }
+        for (int i = 0; i < nOfIt; i++) {
+            System.out.println(polozky[i]);
+        }
+        return null;
+    }
+
+    private static int countLines(String path) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+        int lines = 0;
+        while (bufferedReader.readLine().contains("\\")!=true){
+            lines++;
+        }
+        return lines;
+
+    }
+}
